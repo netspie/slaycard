6222fb25cfcb13fdaec8e5a1b83291708cd2eebc
netspie <dariusz.lusnia@gmail.com>
2024-08-05 17:41:05 +0200

    Fix main page, and clean up card actions

--- src/slaycard-client/src/app/combats/[id]/page.tsx
+++ src/slaycard-client/src/app/combats/[id]/page.tsx
@@ -1,8 +1,14 @@
 "use client";
 
-import { ActionCard, ActionCardRef } from "@/app/components/ActionCard";
+import {
+  ActionCard,
+  ActionCardRef,
+  strToShadowColor,
+} from "@/app/components/ActionCard";
 import { Card, CardProps, CardRef } from "@/app/components/Card";
 import { useCombatState } from "@/app/state/CombatState";
+import Image from "next/image";
+import Link from "next/link";
 import { useEffect, useState } from "react";
 
 const cardsData: string[] = [
@@ -80,65 +86,72 @@ export default function CombatPage() {
   };
 
   return (
-    <div className="flex w-full h-full justify-center p-4 select-none flex-col">
-      <div className="w-full h-[80%] flex flex-col gap-1 py-10">
-        <div className="relative h-full flex gap-2 justify-center">
-          {enemyDeck.characterCards.map((card) => (
-            <Card
-              key={card.id}
-              id={card.id}
-              name={card.name}
-              isOfPlayer={false}
-              isSelected={false}
-              hp={card.hp}
-              energy={card.energy}
-              attack={card.attack}
-              imagePath={`/${cardsData[card.imageIndex]}`}
-              ref={(ref) =>
-                !cards.find((x) => x?.props.id === card.id) && cards.push(ref)
-              }
-            />
-          ))}
-        </div>
-        <div className="relative h-full flex gap-1 justify-center"></div>
-        <div className="relative h-full flex gap-2 justify-center items-center overflow-clip">
-          {playerTurnState.selectedCardForAction &&
-            playerDeck.characterCards
-              .find((c) => c.id === playerTurnState.selectedCardForAction?.id)
-              ?.actionCards?.map((card, i) => (
-                <ActionCard
+    <>
+      <div className="flex flex-col w-full h-full pt-8">
+        <div className="flex w-full h-full justify-center p-4 select-none flex-col">
+          <div className="w-full h-[80%] flex flex-col gap-1 py-10">
+            <div className="relative h-full flex gap-2 justify-center">
+              {enemyDeck.characterCards.map((card) => (
+                <Card
                   key={card.id}
                   id={card.id}
                   name={card.name}
-                  index={i}
-                  color={card.color}
-                  imagePath={`/${actionCardsData[card.imageIndex]}`}
-                  targets={cards}
+                  isOfPlayer={false}
+                  isSelected={false}
+                  hp={card.hp}
+                  energy={card.energy}
+                  attack={card.attack}
+                  imagePath={`/${cardsData[card.imageIndex]}`}
                   ref={(ref) =>
-                    !actionCards.find((x) => x?.props.id === card.id) &&
-                    actionCards.push(ref)
+                    !cards.find((x) => x?.props.id === card.id) &&
+                    cards.push(ref)
                   }
                 />
               ))}
-        </div>
-        <div className="relative h-full flex gap-2 justify-center">
-          {playerDeck.characterCards.map((card) => (
-            <Card
-              key={card.id}
-              id={card.id}
-              name={card.name}
-              isOfPlayer={true}
-              isSelected={playerTurnState.selectedCardForAction?.id === card.id}
-              onSelected={onCardSelected}
-              hp={card.hp}
-              energy={card.energy}
-              attack={card.attack}
-              imagePath={`/${cardsData[card.imageIndex]}`}
-            />
-          ))}
-        </div>
-      </div>
-      {/* <div className="scroll-view relative w-full h-[20%] flex flex-col gap-1 items-center overflow-x-auto">
+            </div>
+            <div className="relative h-full flex gap-1 justify-center"></div>
+            <div className="relative h-full flex gap-2 justify-center items-center overflow-clip">
+              {playerTurnState.selectedCardForAction &&
+                playerDeck.characterCards
+                  .find(
+                    (c) => c.id === playerTurnState.selectedCardForAction?.id
+                  )
+                  ?.actionCards?.map((card, i) => (
+                    <ActionCard
+                      key={card.id}
+                      id={card.id}
+                      name={card.name}
+                      index={i}
+                      color={card.color}
+                      imagePath={`/${actionCardsData[card.imageIndex]}`}
+                      targets={cards}
+                      ref={(ref) =>
+                        !actionCards.find((x) => x?.props.id === card.id) &&
+                        actionCards.push(ref)
+                      }
+                    />
+                  ))}
+            </div>
+            <div className="relative h-full flex gap-2 justify-center">
+              {playerDeck.characterCards.map((card) => (
+                <Card
+                  key={card.id}
+                  id={card.id}
+                  name={card.name}
+                  isOfPlayer={true}
+                  isSelected={
+                    playerTurnState.selectedCardForAction?.id === card.id
+                  }
+                  onSelected={onCardSelected}
+                  hp={card.hp}
+                  energy={card.energy}
+                  attack={card.attack}
+                  imagePath={`/${cardsData[card.imageIndex]}`}
+                />
+              ))}
+            </div>
+          </div>
+          {/* <div className="scroll-view relative w-full h-[20%] flex flex-col gap-1 items-center overflow-x-auto">
           <span className="text-xs text-white"><strong>Ellesandra</strong> damaged <strong>3</strong> to <strong>Imp</strong></span>
           <span className="text-xs text-white"><strong>Ellesandra</strong> damaged <strong>2</strong> to <strong>Goblin</strong></span>
           <span className="text-xs text-white"><strong>Goblin</strong> heals <strong>3</strong> hp</span>
@@ -152,6 +165,8 @@ export default function CombatPage() {
           <span className="text-xs text-white"><strong>Goblin</strong> heals <strong>3</strong> hp</span>
           <span className="text-xs text-white"><strong>Imp</strong> damaged <strong>1</strong> to <strong>Garmir</strong></span>
       </div> */}
-    </div>
+        </div>
+      </div>
+    </>
   );
 }
--- src/slaycard-client/src/app/layout.tsx
+++ src/slaycard-client/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Playfair } from "next/font/google";
 import "./globals.css";
+import Image from "next/image";
+import Link from "next/link";
 
 const inter = Playfair({ subsets: ["latin"] });
 
@@ -20,7 +22,25 @@ export default function RootLayout({
   return (
     <html lang="en" className={htmlClass}>
       <body className={bodyClass}>
-        <div className="fixed bg-repeat w-full h-full bg-[length:512px_512px]  bg-black"></div>
+        <div className="fixed flex p-8">
+          <Link
+            href="/"
+            className="absolute left-10 text-white text-2xl p-0 m-0 z-[1] uppercase font-bold"
+          >
+            Slaycard
+          </Link>
+        </div>
+        <div className="fixed w-full h-full -z-50">
+          <Image
+            className="absolute w-full h-full -z-50 pointer-events-none"
+            src="/bgs/battle-bg.jpg"
+            alt="Dope"
+            layout="fill"
+            objectFit="cover"
+          />
+          <div className="bg-black w-full h-full opacity-[95%]" />
+        </div>
+        <div className="fixed w-full h-full -z-10"></div>
         {children}
       </body>
     </html>
