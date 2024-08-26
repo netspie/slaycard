98d4e7229afa2dead0926b865a3943231d76c176
netspie <dariusz.lusnia@gmail.com>
2024-08-26 22:39:13 +0200

    Fix .gitignore

---
+++ src/slaycard-server/Slaycard.Api/Features/Combats/Domain/Artifacts/AttackArtifact.cs
@@ -0,0 +1,43 @@
+﻿using Core.Domain;
+using Slaycard.Api.Features.Combats.Domain.Events;
+
+namespace Slaycard.Api.Features.Combats.Domain.Artifacts;
+
+public record AttackArtifact(ArtifactId Id) : Artifact(Id)
+{
+    public override IDomainEvent[] ApplyToTarget(
+        ApplyArtifactArgs args, Random? random = null)
+    {
+        var origin = args.OriginUnit.CombatStats;
+        var targets = args.TargetUnits.Map(u => u.CombatStats).ToArray();
+
+        return args.TargetUnits
+            .Map<Unit, IDomainEvent>(targetUnit =>
+            {
+                var target = targetUnit.CombatStats;
+
+                var isHit = CombatFormulas.CalculateIfHit(origin.Accuracy, target.Dodge, random);
+                if (!isHit) 
+                    return new MissedEvent(args.BattleId, args.OriginPlayer.Id, args.OriginUnit.Id, targetUnit.Id);
+
+                var damage = CombatFormulas.CalculateDamage(origin.Attack, target.Defence, out var damageRange, random);
+
+                bool isCriticalHit = CombatFormulas.CalculateIfCriticHit(origin.Critics, target.Critics, out var criticalHitChance, random);
+                damage = isCriticalHit ? CombatFormulas.CalculateCriticalDamage(damage, random) : damage;
+
+                target.HP.Modify(-damage, Id);
+
+                return new DamagedEvent(args.BattleId, args.OriginPlayer.Id, args.OriginUnit.Id, targetUnit.Id, damage, isCriticalHit);
+            })
+            .ToArray();
+    }
+}
+
+public record HealArtifact(ArtifactId Id) : Artifact(Id)
+{
+    public override IDomainEvent[] ApplyToTarget(
+        ApplyArtifactArgs args, Random? random = null)
+    {
+        throw new InvalidOperationException("");
+    }
+}
---
+++ src/slaycard-server/Slaycard.Tests/Features/Combats/UnitTests/Artifacts/AttackArtifactTests.cs
@@ -0,0 +1,34 @@
+﻿using Slaycard.Api.Features.Combats.Domain;
+using Slaycard.Api.Features.Combats.Domain.Artifacts;
+using Slaycard.Api.Features.Combats.Domain.Events;
+
+namespace Slaycard.Tests.Features.Combats.UnitTests.Artifacts;
+
+public class AttackArtifactTests
+{
+    [Test]
+    public void ApplyToTarget()
+    {
+        var unit1 = UnitTests_.CreateUnit("unit-1", statValues: 1);
+        var unit2 = UnitTests_.CreateUnit("unit-2", statValues: 1);
+
+        var player1 = new Player(new PlayerId("player-1"), [unit1]);
+        var player2 = new Player(new PlayerId("player-2"), [unit2]);
+
+        var args = new ApplyArtifactArgs(
+            new BattleId(),
+            player1,
+            unit1,
+            player2,
+            [unit2]);
+
+        var artifact = new AttackArtifact(new ArtifactId("attack"));
+
+        var events = artifact.ApplyToTarget(args, new Random(7));
+        Assert.IsTrue(events.OfType<DamagedEvent>().Any());
+        Assert.IsTrue(unit2.CombatStats.HP.CalculatedValue < unit2.CombatStats.HP.OriginalValue);
+        Assert.IsFalse(events.OfType<MissedEvent>().Any());
+
+        Assert.IsTrue(artifact.ApplyToTarget(args, new Random(8)).OfType<MissedEvent>().Any());
+    }
+}
