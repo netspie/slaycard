19e99e80ea34262d19fa36ea241cbd832961ae87
NOUTBUK\dariu <dariusz.lusnia@gmail.com>
2024-08-27 00:55:35 +0200

    Add initial integration test

---
+++ src/slaycard-server/Slaycard.Tests/Features/Combats/IntegrationTests/BattleTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using System;
+using System.Net;
+using System.Net.Http.Json;
+namespace Slaycard.Tests.Features.Combats.IntegrationTests;
+
+public class BattleTests
+{
+    private WebApplicationFactory<Program> _app;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _app = new WebApplicationFactory<Program>().WithWebHostBuilder(_ => {});
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        _app?.Dispose();
+    }
+
+    [Test]
+    public async Task StartBotBattle()
+    {
+        var client = _app.CreateClient();
+
+        var response = await client.PostAsJsonAsync("/battles", new { });
+
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.BadRequest));
+    }
+}
