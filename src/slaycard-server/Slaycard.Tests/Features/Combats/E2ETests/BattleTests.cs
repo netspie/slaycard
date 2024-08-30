8661e1891237c5f3102dae1df55d01f87c60bf71
NOUTBUK\dariu <dariusz.lusnia@gmail.com>
2024-08-31 00:47:02 +0200

    Add initials battle E2E test

---
+++ src/slaycard-server/Slaycard.Tests/Features/Combats/E2ETests/BattleTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net.Http.Json;
+using System.Net;
+using Microsoft.Extensions.DependencyInjection;
+using Slaycard.Api.Features.Combats.Domain;
+
+namespace Slaycard.Tests.Features.Combats.E2ETests;
+
+public class BattleTests
+{
+    private WebApplicationFactory<Program> _factory;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _factory = new WebApplicationFactory<Program>()
+            .WithWebHostBuilder(
+                builder => 
+                    builder.ConfigureServices(services =>
+                    {
+                        services.AddScoped(sp => 
+                            new RandomizerConfiguration(AlwaysHit: true));
+                    }));
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        _factory?.Dispose();
+    }
+
+    [Test]
+    public async Task PerformBattle()
+    {
+        var client = _factory.CreateClient();
+
+        var response = await client.PostAsJsonAsync(
+            "/battles/startRandomPvE",
+            new { playerId = Guid.NewGuid().ToString() });
+
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+    }
+}
